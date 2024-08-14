46f879f3044525aaa97f1268a309335aedbb33f8
Federico Carrozzino <federicocarrozzino98@gmail.com>
2024-08-14 12:13:19 +0200

    created bindings interface

--- lib/sphincs_plus.dart
+++ lib/sphincs_plus.dart
@@ -1,37 +1,11 @@
-
-import 'dart:async';
+import 'dart:convert';
 import 'dart:ffi';
 import 'dart:io';
-import 'dart:isolate';
+import 'dart:typed_data';
+
+import 'package:ffi/ffi.dart';
 
 import 'sphincs_plus_bindings_generated.dart';
-
-/// A very short-lived native function.
-///
-/// For very short-lived functions, it is fine to call them on the main isolate.
-/// They will block the Dart execution while running the native function, so
-/// only do this for native functions which are guaranteed to be short-lived.
-int sum(int a, int b) => _bindings.sum(a, b);
-
-/// A longer lived native function, which occupies the thread calling it.
-///
-/// Do not call these kind of native functions in the main isolate. They will
-/// block Dart execution. This will cause dropped frames in Flutter applications.
-/// Instead, call these native functions on a separate isolate.
-///
-/// Modify this to suit your own use case. Example use cases:
-///
-/// 1. Reuse a single isolate for various different kinds of requests.
-/// 2. Use multiple helper isolates for parallel execution.
-Future<int> sumAsync(int a, int b) async {
-  final SendPort helperIsolateSendPort = await _helperIsolateSendPort;
-  final int requestId = _nextSumRequestId++;
-  final _SumRequest request = _SumRequest(requestId, a, b);
-  final Completer<int> completer = Completer<int>();
-  _sumRequests[requestId] = completer;
-  helperIsolateSendPort.send(request);
-  return completer.future;
-}
 
 const String _libName = 'sphincs_plus';
 
@@ -52,80 +26,134 @@ final DynamicLibrary _dylib = () {
 /// The bindings to the native functions in [_dylib].
 final SphincsPlusBindings _bindings = SphincsPlusBindings(_dylib);
 
+class SphincsPlus {
+  /// Returns the length of a secret key
+  int get skLength => _bindings.crypto_sign_secretkeybytes();
 
-/// A request to compute `sum`.
-///
-/// Typically sent from one isolate to another.
-class _SumRequest {
-  final int id;
-  final int a;
-  final int b;
+  /// Returns the length of a public key
+  int get pkLength => _bindings.crypto_sign_publickeybytes();
 
-  const _SumRequest(this.id, this.a, this.b);
-}
+  /// Returns the length of a signature
+  int get signatureLength => _bindings.crypto_sign_bytes();
 
-/// A response with the result of `sum`.
-///
-/// Typically sent from one isolate to another.
-class _SumResponse {
-  final int id;
-  final int result;
+  /// Returns the length of the seed required to generate a key pair
+  int get seedLength => _bindings.crypto_sign_seedbytes();
 
-  const _SumResponse(this.id, this.result);
-}
+  /// Generates a SPHINCS+ key pair (public key, secret key)
+  (Uint8List, Uint8List) generateKeyPair({String? seed}) {
+    final pkPointer = calloc.allocate<UnsignedChar>(pkLength);
+    final skPointer = calloc.allocate<UnsignedChar>(skLength);
 
-/// Counter to identify [_SumRequest]s and [_SumResponse]s.
-int _nextSumRequestId = 0;
-
-/// Mapping from [_SumRequest] `id`s to the completers corresponding to the correct future of the pending request.
-final Map<int, Completer<int>> _sumRequests = <int, Completer<int>>{};
-
-/// The SendPort belonging to the helper isolate.
-Future<SendPort> _helperIsolateSendPort = () async {
-  // The helper isolate is going to send us back a SendPort, which we want to
-  // wait for.
-  final Completer<SendPort> completer = Completer<SendPort>();
-
-  // Receive port on the main isolate to receive messages from the helper.
-  // We receive two types of messages:
-  // 1. A port to send messages on.
-  // 2. Responses to requests we sent.
-  final ReceivePort receivePort = ReceivePort()
-    ..listen((dynamic data) {
-      if (data is SendPort) {
-        // The helper isolate sent us the port on which we can sent it requests.
-        completer.complete(data);
-        return;
+    if (seed != null) {
+      final seedPointer = calloc.allocate<UnsignedChar>(seedLength);
+      final seedBytes = utf8.encode(seed);
+      for (var i = 0; i < seedLength; i++) {
+        seedPointer[i] = seedBytes[i];
       }
-      if (data is _SumResponse) {
-        // The helper isolate sent us a response to a request we sent.
-        final Completer<int> completer = _sumRequests[data.id]!;
-        _sumRequests.remove(data.id);
-        completer.complete(data.result);
-        return;
-      }
-      throw UnsupportedError('Unsupported message type: ${data.runtimeType}');
-    });
+      _bindings.crypto_sign_seed_keypair(pkPointer, skPointer, seedPointer);
+      calloc.free(seedPointer);
+    } else {
+      _bindings.crypto_sign_keypair(pkPointer, skPointer);
+    }
 
-  // Start the helper isolate.
-  await Isolate.spawn((SendPort sendPort) async {
-    final ReceivePort helperReceivePort = ReceivePort()
-      ..listen((dynamic data) {
-        // On the helper isolate listen to requests and respond to them.
-        if (data is _SumRequest) {
-          final int result = _bindings.sum_long_running(data.a, data.b);
-          final _SumResponse response = _SumResponse(data.id, result);
-          sendPort.send(response);
-          return;
-        }
-        throw UnsupportedError('Unsupported message type: ${data.runtimeType}');
-      });
+    final pk = Uint8List.fromList(
+      (pkPointer as Pointer<Uint8>).asTypedList(pkLength),
+    );
 
-    // Send the port to the main isolate on which we can receive requests.
-    sendPort.send(helperReceivePort.sendPort);
-  }, receivePort.sendPort);
+    final sk = Uint8List.fromList(
+      (skPointer as Pointer<Uint8>).asTypedList(skLength),
+    );
 
-  // Wait until the helper isolate has sent us back the SendPort on which we
-  // can start sending requests.
-  return completer.future;
-}();
+    calloc
+      ..free(pkPointer)
+      ..free(skPointer);
+
+    return (pk, sk);
+  }
+
+  /// Returns the signed message
+  Uint8List sign({
+    required String message,
+    required Uint8List secretKey,
+  }) {
+    if (secretKey.length != skLength) {
+      throw Exception('Invalid secret key!');
+    }
+    final mLength = message.length;
+    final smLength = mLength + signatureLength;
+
+    final smlen = calloc.allocate<UnsignedLongLong>(1);
+    smlen.value = smLength;
+
+    final sm = calloc.allocate<UnsignedChar>(smLength);
+
+    final m = calloc.allocate<UnsignedChar>(mLength);
+    final msgBytes = utf8.encode(message);
+    for (var i = 0; i < mLength; i++) {
+      m[i] = msgBytes[i];
+    }
+
+    final sk = calloc.allocate<UnsignedChar>(skLength);
+    for (var i = 0; i < skLength; i++) {
+      sk[i] = secretKey[i];
+    }
+    _bindings.crypto_sign(sm, smlen, m, mLength, sk);
+
+    final signedMessage = Uint8List.fromList(
+      (sm as Pointer<Uint8>).asTypedList(smLength),
+    );
+
+    calloc
+      ..free(sm)
+      ..free(smlen)
+      ..free(m)
+      ..free(sk);
+
+    return signedMessage;
+  }
+
+  /// Verifies a given signed message under a given public key
+  bool verify({
+    required String message,
+    required Uint8List signedMessage,
+    required Uint8List publicKey,
+  }) {
+    final mLength = message.length;
+    final smLength = mLength + signatureLength;
+    if (signedMessage.length != smLength) {
+      return false;
+    }
+    if (publicKey.length != pkLength) {
+      throw Exception('Invalid public key!');
+    }
+
+    final mlen = calloc.allocate<UnsignedLongLong>(1);
+    mlen.value = mLength;
+
+    final m = calloc.allocate<UnsignedChar>(mLength);
+    final msgBytes = utf8.encode(message);
+    for (var i = 0; i < mLength; i++) {
+      m[i] = msgBytes[i];
+    }
+
+    final sm = calloc.allocate<UnsignedChar>(smLength);
+    for (var i = 0; i < smLength; i++) {
+      sm[i] = signedMessage[i];
+    }
+
+    final pk = calloc.allocate<UnsignedChar>(pkLength);
+    for (var i = 0; i < pkLength; i++) {
+      pk[i] = publicKey[i];
+    }
+
+    final result = _bindings.crypto_sign_open(m, mlen, sm, smLength, pk);
+
+    calloc
+      ..free(m)
+      ..free(mlen)
+      ..free(sm)
+      ..free(pk);
+
+    return result == 0;
+  }
+}
